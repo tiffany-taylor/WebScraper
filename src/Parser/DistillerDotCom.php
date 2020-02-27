abbd9d6ef252aff096148804ba4d8729318614c2
Tiffany <tiffany.k.taylor@gmail.com>
2020-02-27 11:33:00 -0600

    use xpath utility to normalize xpath query

--- src/Parser/DistillerDotCom.php
+++ src/Parser/DistillerDotCom.php
@@ -4,6 +4,8 @@
 namespace WebScraper\Parser;
 
 
+use function Room11\DOMUtils\xpath_html_class;
+
 class DistillerDotCom
 {
     public function parse(\DOMDocument $dom)
@@ -13,10 +15,11 @@ class DistillerDotCom
         if (!$this->isBoozeFound($xpath)) {
             return null;
         }
+        return 'found';
     }
 
     private function isBoozeFound(\DOMXPath $xpath): bool
     {
-        return (bool) $xpath->evaluate("//ol[@class='spirits']/li[@class='spirit']")->length;
+        return (bool) $xpath->evaluate('//ol['.xpath_html_class('spirits').']/li['.xpath_html_class('spirit').']')->length;
     }
 }
