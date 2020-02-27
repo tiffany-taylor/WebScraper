fbf009dcbbceb841f8d335e4bb5de420536c122e
Tiffany <tiffany.k.taylor@gmail.com>
2020-02-27 10:45:58 -0600

    add temp test file

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace WebScraper;
+
+use Amp\Http\Client\HttpClientBuilder;
+use WebScraper\Http\Client;
+use function Amp\Promise\wait;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$scraper = new Scraper(new Client(HttpClientBuilder::buildDefault()));
+
+var_dump(wait($scraper->search('nonexistingcommand')));
